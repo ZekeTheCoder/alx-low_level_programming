65e34df387decab470b5146e9c7f7fe53900b6ad
Thabang Maphangisane <thabangh647@gmail.com>
2023-09-12 23:20:19 +0200

    updated the main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +13,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
 void print_times_table(int n);
 
 #endif
