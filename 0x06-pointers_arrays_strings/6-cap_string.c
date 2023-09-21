274f709aeb6eef6b26352722b8823c9167c35a8d
Thabang Maphangisane <thabangh647@gmail.com>
2023-09-21 23:00:01 +0200

    Fix: account for the word seperators in File: 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * cap_string -  changes all lowercase letters of a string to uppercase.
+ * cap_string -  capitalizes all words of a string.
+ * Separators of words:
+ * space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }
  *
  * @s: input string.
  *
@@ -11,29 +13,25 @@
 char *cap_string(char *s)
 {
 	int i;
+	int count = 0;
+	/*separator characters asci values*/
+	int sep_characters[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (i == 0)
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i] >= 's' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			continue;
-		}
-		if (s[i] == ' ')
-		{
-			i++;
-			if (*(s + i) >= 's' && *(s + i) <= 'z')
+			if (*(s + count) == sep_characters[i])
 			{
-				*(s + i) = *(s + i) - 32;
-				continue;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-		else
-		{
-			if (s[i] >= 'A' && s[i] <= 'Z')
-				s[i] = s[i] + 32;
-		}
+		count++;
 	}
 	return (s);
 }
