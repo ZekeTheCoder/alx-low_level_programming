cb304fa24097ff968e9a67a34b8b8f53be68f4be
Thabang Maphangisane <thabangh647@gmail.com>
2023-10-24 21:35:43 +0200

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,36 +20,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *h;
 	listint_t *new_node;
 
-	if (!head || !*head)
+	h = *head;
+
+	while (idx != 0 && i < idx - 1 && h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	if (h == NULL && idx != 0)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	h = *head;
 
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (*head);
 	}
-
-	while (h)
+	else
 	{
-		if (i == (idx - 1))
-		{
-			new_node->next = h->next;
-			h->next = new_node;
-			return (new_node);
-		}
-		h = h->next;
-		i++;
+		new_node->next = h->next;
+		h->next = new_node;
 	}
 
-	free(new_node);
-	return (NULL);
+	return (new_node);
 }
